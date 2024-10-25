ea51c0228e4e5e75addd4189babf15d6d2e56150
John Zhang <johnz.zhang@mail.utoronto.ca>
2024-10-25 16:26:19 -0400

    Add config files and wrote app script for an ML endpoint

---
+++ application.py
@@ -0,0 +1,27 @@
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.naive_bayes import MultinomialNB
+import pickle
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route('predict', methods=['POST'])
+def predict():
+    data = request.get_json()
+    transformed_text = vectorizer.transform(data['text'])
+    prediction = model.predict(transformed_text)
+    return jsonify({'prediction': prediction.tolist()})
+
+def load_model():
+    loaded_model = None
+    with open('basic_classifier.pkl', 'rb') as fid:
+        loaded_model = pickle.load(fid)
+
+    vectorizer = None
+    with open('vectorizer.pkl', 'rb') as fid:
+        vectorizer = pickle.load(fid)
+    return loaded_model, vectorizer
+
+if __name__ == "__main__":
+    model, vectorizer = load_model()
+    app.run(port=5000, debug=True)
